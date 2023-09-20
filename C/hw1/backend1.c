529258b3f1d2bf7acaccbf3a69c4845c362b98e0
Estrel <kestrel3905@naver.com>
2023-09-20 12:19:57 +0900

    20230920

--- C/hw1/backend1.c
+++ C/hw1/backend1.c
@@ -2,11 +2,19 @@
 
 #include <stdio.h>
 
-#define NUMBER_OF_RECORDS 100
+#define NUMBER_OF_RECORDS 5
+
+int search_index(char[3]);
+void print_name(int);
+void print_number(int);
+void print_data(char *, int);
+
+// External Variables
 
 char data_name[NUMBER_OF_RECORDS][3];
 char data_number[NUMBER_OF_RECORDS][4];
-int k = 0;
+
+int k = 0;  // index of the first empty slot
 
 void add(char name[3], char number[4]) {
     if (k < NUMBER_OF_RECORDS) {
@@ -23,29 +31,25 @@ void add(char name[3], char number[4]) {
         printf(" was successfully added!\n");
         k++;
     } else
-        printf("Can¡¯t add. Address book is full.\n");
+        printf("Can't add. Address book is full.\n");
 }
 
+// Returns the index of the found record.
+// Returns -1, if not found.
 int search_index(char name[3]) {
     int i;
 
-    for (i = 0; i < k; i++) {
-        int j;
-
-        for (j = 0; j < 3; j++)
-            if (data_name[i][j] != name[j]) break;
-        if (j == 3) break;
-    }
-    if (i == k) return -1;
-    return i;
+    for (i = 0; i < k; i++)
+        if (data_name[i][0] == name[0] && data_name[i][1] == name[1] && data_name[i][2] == name[2]) return i;
+    return -1;
 }
 
 void search(char name[3]) {
     int s_result;
-    s_result = search_index(name);
 
+    s_result = search_index(name);
     if (s_result == -1)
-        printf("%c%c%c doesn't exist.\n", name[0], name[1], name[2]);
+        printf("Couldn't find the name.\n");
     else {
         print_name(s_result);
         printf(" : ");
@@ -59,7 +63,7 @@ void delete(char name[3]) {
     s_result = search_index(name);
 
     if (s_result == -1)
-        printf("Can't delete. %c%c%c doesn't exist.\n", name[0], name[1], name[2]);
+        printf("Couldn't find the name.\n");
     else {
         for (int i = s_result; i < k; i++) {
             data_name[i][0] = data_name[i + 1][0];
@@ -69,25 +73,25 @@ void delete(char name[3]) {
             data_number[i][1] = data_number[i + 1][1];
             data_number[i][2] = data_number[i + 1][2];
             data_number[i][3] = data_number[i + 1][3];
-            k--;
         }
-        printf("%c%c%c was successfully deleted!\n", name[0], name[1], name[2]);
+        k--;
+        printf("The name was successfully deleted.\n");
     }
 }
 
+// Prints ith name.
 void print_name(int i) { print_data(data_name[i], 3); }
 
+// Prints ith number.
 void print_number(int i) { print_data(data_number[i], 4); }
 
-void print_data(char* s, int n) {
+void print_data(char *s, int n) {
     int i;
-
     for (i = 0; i < n; i++) putchar(s[i]);
 }
 
 void print_list() {
     int i;
-
     for (i = 0; i < k; i++) {
         print_name(i);
         printf(" : ");
